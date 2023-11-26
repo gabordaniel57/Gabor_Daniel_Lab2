97bf1b7d904f96bee22e228bed5f0b105c997879
dgabor <daniel.cosmin.gabor@reea.net>
2023-11-26 11:54:45 +0200

    Add project files.

---
+++ Data/Gabor_Daniel_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gabor_Daniel_Lab2.Models;
+
+namespace Gabor_Daniel_Lab2.Data
+{
+    public class Gabor_Daniel_Lab2Context : DbContext
+    {
+        public Gabor_Daniel_Lab2Context (DbContextOptions<Gabor_Daniel_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gabor_Daniel_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Gabor_Daniel_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Gabor_Daniel_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+        public string PublisherAdress { get; set; }
+    }
+}
